d7efc10907ecbd5b41ade3425f870fad337c02c8
DimaSauchanka <dimasauchanka@gmail.com>
2015-11-29 17:58:50 +0300

    Simple connection to database

--- News_Project/views.py
+++ News_Project/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.db import connection
 # Create your views here.
 
 def index(request):
     header = 'Header!'
-    return render_to_response('index.html', {'header': header})
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM News')
+    data = cursor.fetchall()
+    print(data)
+
+    return render_to_response('index.html', {'header': header, 'news': data})
