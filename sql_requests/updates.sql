766529f0bd00446cbb5360b77b512373b57bbb6f
IIIJIaHr <midav.lis@gmail.com>
2016-05-15 16:16:51 +0300

    Fix some mistakes in updates.sql.

--- sql_requests/updates.sql
+++ sql_requests/updates.sql
@@ -4,7 +4,7 @@ BEGIN
 	UPDATE NEWS SET Number_likes = Number_likes + 1 WHERE News_id = p_article_id;
 	commit;
 END INC_NUMBERS_LIKE;
-
+/
 
 CREATE OR REPLACE PROCEDURE INC_NUMBERS_LIKE_JOURNALIST(p_article_id IN NUMBER)
 AS
@@ -13,10 +13,11 @@ BEGIN
 		WHERE News_id = p_article_id;
 	commit;
 END INC_NUMBERS_LIKE_JOURNALIST;
-
+/
 
 CREATE OR REPLACE PROCEDURE MAKE_JOURNALIST(p_user_id IN NUMBER)
 AS
 BEGIN
 	UPDATE USERS SET Status_id = 2 WHERE User_id = p_user_id;
+	commit;
 END MAKE_JOURNALIST;
