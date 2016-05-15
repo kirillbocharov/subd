e161e28de242fd6d8a2b6d67a36f047d52ee3b80
IIIJIaHr <midav.lis@gmail.com>
2016-05-15 16:11:38 +0300

    Fix some mistakes in selects.sql.

--- sql_requests/selects.sql
+++ sql_requests/selects.sql
@@ -9,7 +9,7 @@ BEGIN
 		p_is_exists := 0;
 	END IF;
 END HAS_LOGIN;
-
+/
 
 CREATE OR REPLACE PROCEDURE HAS_EMAIL(p_email IN VARCHAR2, p_out_is_exists OUT NUMBER)
 AS
@@ -22,28 +22,28 @@ BEGIN
 		p_out_is_exists := 0;
 	END IF;
 END HAS_EMAIL;
-
+/
 
 CREATE OR REPLACE PROCEDURE VERIFY_USER(p_email IN VARCHAR2, p_password IN VARCHAR2, p_out_user_id OUT NUMBER)
 AS
 	v_is_email_exists NUMBER;
-	v_pass VARCHAR2;
+	v_pass VARCHAR2(500);
 BEGIN
 	HAS_EMAIL(p_email, v_is_email_exists);
-	IF v_is_email_exists == 1 THEN
-		SELECT Password INTO v_pass, User_id INTO p_out_user_id 
+		SELECT USERS.Password, User_id INTO v_pass, p_out_user_id 
 			FROM USERS WHERE Email=p_email;
 		IF v_pass != p_password THEN
 			p_out_user_id := 0;
+		END IF;
 END VERIFY_USER;
-
+/
 
 CREATE OR REPLACE PROCEDURE GET_NEWS(p_out_cursor OUT sys_refcursor)
 AS
 BEGIN
 	OPEN p_out_cursor FOR SELECT * FROM NEWS WHERE Is_sand=0;
 END GET_NEWS;
-
+/
 
 CREATE OR REPLACE PROCEDURE GET_NEWS_BY_NAME(p_user_name IN VARCHAR2, p_out_cursor OUT sys_refcursor)
 AS
@@ -51,14 +51,14 @@ BEGIN
 	OPEN p_out_cursor FOR SELECT * FROM NEWS N, USERS U 
 		WHERE N.User_id = U.User_id AND U.User_name = p_user_name;
 END GET_NEWS_BY_NAME;
-
+/
 
 CREATE OR REPLACE PROCEDURE GET_NAME_USER(p_user_id IN NUMBER, p_out_cursor OUT sys_refcursor)
 AS
 BEGIN
 	OPEN p_out_cursor FOR SELECT * FROM USERS WHERE User_id = p_user_id;
-END GET_NAME_USER
-
+END GET_NAME_USER;
+/
 
 CREATE OR REPLACE PROCEDURE GET_COMMENTS(p_article_id IN NUMBER, p_out_cursor OUT sys_refcursor)
 AS
@@ -67,21 +67,20 @@ BEGIN
 		COMMENTS INNER JOIN USERS ON News_id = p_article_id 
 		AND USERS.User_id = COMMENTS.User_id;
 END GET_COMMENTS;
-
+/
 
 CREATE OR REPLACE PROCEDURE IS_JOURNALIST(p_user_id IN NUMBER, p_out_is_journ OUT NUMBER)
 AS
 	v_status_id NUMBER;
 BEGIN
 	SELECT Status_id INTO v_status_id FROM USERS WHERE User_id = p_user_id;
-	IF Status_id == 2
-	THEN
+	IF v_status_id = 2 THEN
 		p_out_is_journ := 1;
 	ELSE
 		p_out_is_journ := 0;
 	END IF;
 END IS_JOURNALIST;
-
+/
 
 CREATE OR REPLACE PROCEDURE IS_LEFT_LIKE(
 	p_article_id IN NUMBER,
@@ -92,21 +91,20 @@ AS
 BEGIN
 	SELECT COUNT(*) INTO v_count FROM LIKES 
 		WHERE User_id = p_user_id AND News_id = p_article_id;
-		IF v_count == 0
-		THEN
-			p_out_is_left := 0
+		IF v_count = 0 THEN
+			p_out_is_left := 0;
 		ELSE
-			p_out_is_left := 1
+			p_out_is_left := 1;
 		END IF;
 END IS_LEFT_LIKE;
+/
 
-
-CREATE OR REPLACE PROCEDURE GET_CHILDS_ARTICLES(p_out_cursor OUT NUMBER)
+CREATE OR REPLACE PROCEDURE GET_CHILDS_ARTICLES(p_out_cursor OUT sys_refcursor)
 AS
 BEGIN
 	OPEN p_out_cursor FOR SELECT * FROM NEWS WHERE Is_sand = 1;
 END GET_CHILDS_ARTICLES;
-
+/
 
 CREATE OR REPLACE PROCEDURE GET_NUMLIKES_JOURN(p_user_id IN NUMBER, p_out_value OUT NUMBER)
 AS
@@ -114,7 +112,7 @@ BEGIN
 	SELECT SUM(N.Number_likes_journalist) INTO p_out_value FROM USERS U INNER JOIN NEWS N 
 		ON U.User_id = N.User_id AND U.User_id = p_user_id; 
 END GET_NUMLIKES_JOURN;
-
+/
 
 CREATE OR REPLACE PROCEDURE GET_OWNER_ARTICLE(
 	p_article_id IN NUMBER, 
@@ -123,7 +121,7 @@ AS
 BEGIN
 	SELECT User_id INTO p_out_user_id FROM NEWS WHERE News_id = p_article_id;
 END GET_OWNER_ARTICLE;
-
+/
 
 CREATE OR REPLACE PROCEDURE GET_ARTICLE(
 	p_article_id IN NUMBER,
