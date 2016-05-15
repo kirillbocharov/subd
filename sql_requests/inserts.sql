b68554a2988c10ee1d0645756d8c20c5267d912a
IIIJIaHr <midav.lis@gmail.com>
2016-05-15 15:34:41 +0300

    Fix some mistakes.

--- sql_requests/inserts.sql
+++ sql_requests/inserts.sql
@@ -8,7 +8,7 @@ BEGIN
 		VALUES(p_user_name, p_password, p_status_id, p_email);
 		commit;
 END ADD_USER;
-
+/
 
 CREATE OR REPLACE PROCEDURE ADD_NEWS(
 	p_data_sent VARCHAR2,
@@ -23,7 +23,7 @@ BEGIN
 		VALUES(p_data_sent, p_is_sand, p_news, p_header, p_foto, p_number_likes, p_user_id);
 		commit;
 END ADD_NEWS;
-
+/
 
 CREATE OR REPLACE PROCEDURE ADD_COMMENT(
 	p_news_id IN NUMBER, 
@@ -31,11 +31,11 @@ CREATE OR REPLACE PROCEDURE ADD_COMMENT(
 	p_data_sent IN VARCHAR2, 
 	p_text_cmt IN VARCHAR2) AS
 BEGIN
-	INSERT INTO COMMETS(News_id, User_id, Date_sent, Text_cmt)
+	INSERT INTO COMMENTS(News_id, User_id, Date_sent, Text_cmt)
 		VALUES(p_news_id, p_user_id, p_data_sent, p_text_cmt);
 		commit;
 END ADD_COMMENT;
-
+/
 
 CREATE OR REPLACE PROCEDURE ADD_LIKE(
 	p_article_id IN NUMBER,
@@ -43,4 +43,5 @@ CREATE OR REPLACE PROCEDURE ADD_LIKE(
 BEGIN
 	INSERT INTO LIKES(News_id, User_id)
 		VALUES(p_article_id, p_user_id);
+		commit;
 END ADD_LIKE;
